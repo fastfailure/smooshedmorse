276d63fe1e7e65ec67df00bcbe147df41fd0f346
Sandro Pratesi <sandro.pratesi@gmx.com>
2021-04-23 17:54:05 +0200

    run is better in main

--- src/lib.rs
+++ src/lib.rs
@@ -7,31 +7,9 @@ pub mod input;
 pub mod merses;
 pub mod morses;
 
-mod extra1;
-mod extra2;
-mod extra3;
-mod extra4;
+pub mod extra1;
+pub mod extra2;
+pub mod extra3;
+pub mod extra4;
+
 mod wordlist;
-
-use std::process;
-
-use input::Config;
-use input::Method;
-
-pub fn run(config: Config) -> Result<Vec<String>, &'static str> {
-    let res = match config.method {
-        // Method::Encode => encode::encode(config.word.unwrap().clone()),
-        // Method::Decode => decode::decode(config.word.unwrap().clone()),
-        Method::Encode => encode::encode(config.word.unwrap()),
-        Method::Decode => decode::decode(config.word.unwrap()),
-        Method::Extra1 => extra1::run(),
-        Method::Extra2 => extra2::run(),
-        Method::Extra3 => extra3::run(),
-        Method::Extra4 => extra4::run(),
-    };
-    if let Err(e) = res {
-        log::error!("Application error: {}", e);
-        process::exit(1);
-    };
-    res
-}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,37 @@
+use serde_json::json;
 use std::env;
 use std::process;
-use serde_json::json;
 
+use smooshedmorse::decode;
+use smooshedmorse::encode;
+use smooshedmorse::extra1;
+use smooshedmorse::extra2;
+use smooshedmorse::extra3;
+use smooshedmorse::extra4;
 use smooshedmorse::input::get_config;
-use smooshedmorse::run;
+use smooshedmorse::input::Config;
+use smooshedmorse::input::Method;
+
+pub fn run(config: Config) -> Result<Vec<String>, &'static str> {
+    let res = match config.method {
+        Method::Encode => encode::encode(config.word.unwrap()),
+        Method::Decode => decode::decode(config.word.unwrap()),
+        Method::Extra1 => extra1::run(),
+        Method::Extra2 => extra2::run(),
+        Method::Extra3 => extra3::run(),
+        Method::Extra4 => extra4::run(),
+    };
+    if let Err(e) = res {
+        log::error!("Application error: {}", e);
+        process::exit(1);
+    };
+    res
+}
+
+fn print_result(words: Vec<String>) {
+    let json_words = json!(words);
+    println!("{}", json_words);
+}
 
 fn main() {
     env_logger::init();
@@ -13,9 +41,4 @@ fn main() {
         process::exit(1);
     });
     print_result(run(config).unwrap());
-}
-
-fn print_result(words: Vec<String>) {
-    let json_words = json!(words);
-    println!("{}", json_words);
 }
