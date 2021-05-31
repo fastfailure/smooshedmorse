6b07c8a1a4610d276aec033992f9439795d5ea59
Sandro Pratesi <sandro.pratesi@gmx.com>
2021-05-31 23:49:24 +0200

    should be complete, need tests

--- src/permutations.rs
+++ src/permutations.rs
@@ -54,11 +54,11 @@ fn validate_smalpha(smalpha: &str) -> Result<(), &'static str> {
 }
 
 fn smalpha_right_len() -> usize {
-    encode(ALPHABET.into_iter().collect()).unwrap()[0].len()
+    encode(ALPHABET.iter().collect()).unwrap()[0].len()
 }
 
 fn chars_to_smooshedmerse(chars: &[char]) -> Vec<bool> {
-    smooshedmorse_to_merse(&encode(chars.into_iter().collect::<String>()).unwrap()[0])
+    smooshedmorse_to_merse(&encode(chars.iter().collect::<String>()).unwrap()[0])
 }
 
 #[derive(Debug)]
@@ -88,14 +88,14 @@ impl SegmentChars {
             source: chars.to_owned(),
             amount,
             take: vec![],
-            merse_take: chars_to_smooshedmerse(&vec![]),
+            merse_take: chars_to_smooshedmerse(&[]),
             left: vec![],
             permutations,
         }
     }
     fn get_left(&self, source: &[char], taken: &[char]) -> Vec<char> {
         // source - taken
-        let taken_set: HashSet<_> = taken.into_iter().collect();
+        let taken_set: HashSet<_> = taken.iter().collect();
         let difference: Vec<char> = source
             .to_owned()
             .into_iter()
@@ -123,10 +123,10 @@ fn random_alphabet() -> String {
     rc.take.into_iter().collect()
 }
 
-/// Do all rcs match with input?
-fn check_for_match(input: &[bool], rcs: &HashMap<usize, &mut SegmentChars>) -> bool {
+/// Do all segchs match with input?
+fn check_for_match(input: &[bool], segchs: &HashMap<usize, SegmentChars>) -> bool {
     let mut i = 0;
-    for (_, c) in rcs.into_iter() {
+    for (_, c) in segchs.iter() {
         if c.merse_take == input[i..=(i + c.merse_take.len())] {
             return false;
         }
@@ -139,39 +139,37 @@ fn algo(
     input: &[bool],
     increment: usize,
     mut i: usize,
-    rcs: &mut HashMap<usize, &mut SegmentChars>,
+    segchs: &mut HashMap<usize, SegmentChars>,
 ) -> (usize, Option<Vec<Vec<char>>>) {
     log::debug!(
         "Entering algorithm level #{}, source: {:?}, {:?}",
         &i,
         &input,
-        &rcs
+        &segchs
     );
     loop {
-        rcs.get(&i).unwrap().new_perm();
-        if rcs.get(&i).unwrap().take.is_empty() {
+        segchs.get_mut(&i).unwrap().new_perm();
+        if segchs.get(&i).unwrap().take.is_empty() {
             log::trace!("No more combinations, exiting level #{}", i);
             i -= 1;
             return (i, None);
         }
-        if check_for_match(input, rcs) {
+        if check_for_match(input, segchs) {
             log::trace!("Match on segment #{})", i);
-            if rcs.get(&i).unwrap().left.is_empty() {
+            if segchs.get(&i).unwrap().left.is_empty() {
                 log::debug!("Success on level #{}! Creating result", i);
                 let mut char_combi = Vec::new();
                 for ii in 0..=i {
-                    // let matching_seg: &mut SegmentChars = rcs.get(&ii).unwrap();
-                    // char_combi.append(&mut matching_seg.take);
+                    let matching_seg: &mut SegmentChars = segchs.get_mut(&ii).unwrap();
+                    char_combi.append(&mut matching_seg.take);
                     log::trace!("Growing result ({}): {:?}", ii, char_combi);
                 }
                 return (i, Some(vec![char_combi]));
             }
             i += 1;
-            rcs.insert(
-                i,
-                &mut SegmentChars::init(&rcs.get(&i).unwrap().left, increment),
-            );
-            let (i, step) = algo(input, increment, i, rcs);
+            let segch_new = SegmentChars::init(&segchs.get(&i).unwrap().left, increment);
+            segchs.insert(i, segch_new);
+            let (i, step) = algo(input, increment, i, segchs);
             match step {
                 Some(res) => return (i, Some(res)),
                 None => continue,
@@ -182,10 +180,11 @@ fn algo(
 
 fn find_permutations(merse_alpha_perm: &[bool], increment: u8) -> Vec<Vec<char>> {
     let increment: usize = increment as usize;
-    let mut rcs: HashMap<usize, &mut SegmentChars> = HashMap::new();
+    let mut segchs: HashMap<usize, SegmentChars> = HashMap::new();
     let i = 0;
-    rcs.insert(i, &mut SegmentChars::init(&ALPHABET, increment));
-    let (_i, res) = algo(merse_alpha_perm, increment, i, &mut rcs);
+    let segch0 = SegmentChars::init(&ALPHABET, increment);
+    segchs.insert(i, segch0);
+    let (_i, res) = algo(merse_alpha_perm, increment, i, &mut segchs);
     match res {
         None => vec![vec![]], // failure, no match!
         Some(r) => r,
